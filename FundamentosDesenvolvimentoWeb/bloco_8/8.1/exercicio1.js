415041a0dd5eff39997e1f71dadcfe92b1b075ab
Josue Lobo <darksiders20005@gmail.com>
2021-07-15 18:23:52 -0300

    iniciando exercicio

--- FundamentosDesenvolvimentoWeb/bloco_8/8.1/exercicio1.js
+++ FundamentosDesenvolvimentoWeb/bloco_8/8.1/exercicio1.js
@@ -1,16 +1,10 @@
-const employeeGenerator = (fullName) => {
-  const email = fullName.toLowerCase().split(' ').join('_');
-  // toLowercase deixa tudo em minusculo, split quebra a string em um array é vai levar em base o parametro passado para quebrar... no exemplo vai ser os espaços, o join une todo um array é usa o parametro apontado o que vai fazer a divisão no exemplo eo _
-  return { fullName, email: `${email}@trybe.com` };
-};
+const employeeGenerator = ()
 
-const newEmployees = (formataName) => {
+const newEmployees = () => {
   const employees = {
-    id1: formataName('Pedro Guerra'),
-    id2: formataName('Luiza Drumond'),
-    id3: formataName('Carla Paiva'),
-  };
+    id1: employeeGenerator('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: employeeGenerator('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: employeeGenerator('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
   return employees;
 };
-
-console.log(newEmployees(employeeGenerator));
