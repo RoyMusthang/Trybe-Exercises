31e095a9226975e4084e1267bf508accbfeb35f6
Josue Lobo <darksiders20005@gmail.com>
2021-07-31 16:13:37 -0300

    finalizando fixação

--- DesenvolvimentoFrontEnd/bloco_12/fixar/fancy-buttons/src/App.js
+++ DesenvolvimentoFrontEnd/bloco_12/fixar/fancy-buttons/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
 
-function handleClick0() { // função que imprime o texto
-  console.log('clicou0!')
-}
-
-function handleClick1() { // função que imprime o texto
-  console.log('clicou1!')
-}
-
-function handleClick2() { // função que imprime o texto
-  console.log('clicou2!')
-}
-
 class App extends React.Component {
+  constructor() {
+    super()
+    this.handleClick0 = this.handleClick0.bind(this);
+    this.handleClick1 = this.handleClick1.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+
+    this.state = {
+      handleClick0: 0,
+      handleClick1: 0,
+      handleClick2: 0,
+    }
+  }
+
+  handleClick0 = () => {
+    this.setState((estadoAnterior, _props) => ({
+      handleClick0: estadoAnterior.handleClick0 + 1
+    }))
+  }
+  
+  handleClick1 = () => {
+    this.setState((estadoAnterior, _props) => ({
+      handleClick1: estadoAnterior.handleClick1 + 1
+    }))
+  }
+  
+  handleClick2 = () => {
+    this.setState((estadoAnterior, _props) => ({
+      handleClick2: estadoAnterior.handleClick2 + 1
+    }))
+  }
+
   render() {
     return (
       <>
-        <button onClick={ handleClick0 }>click aqui </button>
-        <button onClick={ handleClick1 }>click aqui </button>
-        <button onClick={ handleClick2 }>click aqui </button>
+        <button onClick={ this.handleClick0 }>{this.state.handleClick0}</button> {/* o conteudo eo contador */}
+        <button onClick={ this.handleClick1 }>{this.state.handleClick1}</button>
+        <button onClick={ this.handleClick2 }>{this.state.handleClick2}</button>
       </>
-    ); // o evento onClick chama a função acima
+    );
   }
 }
 
