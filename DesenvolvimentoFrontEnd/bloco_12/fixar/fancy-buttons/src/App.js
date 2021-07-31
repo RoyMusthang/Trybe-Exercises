65b656da61d258bce022385c4e6b1ed1a3d9fed3
Josue Lobo <darksiders20005@gmail.com>
2021-07-31 16:44:46 -0300

    finalizando revisão

--- DesenvolvimentoFrontEnd/bloco_12/fixar/fancy-buttons/src/App.js
+++ DesenvolvimentoFrontEnd/bloco_12/fixar/fancy-buttons/src/App.js
@@ -3,41 +3,53 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super()
+    this.state = {
+      Click0: 0,
+      Click1: 0,
+      Click2: 0,
+    }
+
     this.handleClick0 = this.handleClick0.bind(this);
     this.handleClick1 = this.handleClick1.bind(this);
     this.handleClick2 = this.handleClick2.bind(this);
-
-    this.state = {
-      handleClick0: 0,
-      handleClick1: 0,
-      handleClick2: 0,
-    }
   }
 
   handleClick0 = () => {
     this.setState((estadoAnterior, _props) => ({
-      handleClick0: estadoAnterior.handleClick0 + 1
-    }))
+    Click0: estadoAnterior.Click0 + 1
+    }), () => {
+    console.log(this.buttonColor(this.state.Click0));
+    });
   }
   
   handleClick1 = () => {
     this.setState((estadoAnterior, _props) => ({
-      handleClick1: estadoAnterior.handleClick1 + 1
-    }))
+      Click1: estadoAnterior.Click1 + 1
+    }), () => {
+      console.log(this.buttonColor(this.state.Click1));
+    });
   }
   
   handleClick2 = () => {
     this.setState((estadoAnterior, _props) => ({
-      handleClick2: estadoAnterior.handleClick2 + 1
-    }))
+      Click2: estadoAnterior.Click2 + 1
+    }), () => {
+      console.log(this.buttonColor(this.state.Click2));
+    });
+  }
+
+  buttonColor(num) {
+    if (num % 2 === 0) return 'green';
+    return 'white';
   }
 
   render() {
+    const { Click0, Click1, Click2 } = this.state;
     return (
       <>
-        <button onClick={ this.handleClick0 }>{this.state.handleClick0}</button> {/* o conteudo eo contador */}
-        <button onClick={ this.handleClick1 }>{this.state.handleClick1}</button>
-        <button onClick={ this.handleClick2 }>{this.state.handleClick2}</button>
+        <button style={{ backgroundColor: this.buttonColor(Click0) }} onClick={ this.handleClick0 }>{this.state.Click0}</button> {/* o conteudo eo contador */}
+        <button style={{ backgroundColor: this.buttonColor(Click1) }}  onClick={ this.handleClick1 }>{this.state.Click1}</button> 
+        <button style={{ backgroundColor: this.buttonColor(Click2) }}  onClick={ this.handleClick2 }>{this.state.Click2}</button> 
       </>
     );
   }
